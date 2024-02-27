75d37c10f365ccc53425db8887f07c1e174f0644
oluceps <i@oluceps.uk>
2024-02-27 13:39:22 +0800

    + bootstrap

---
+++ README.md
@@ -0,0 +1,7 @@
+# Telegram User Online Keeper
+
+Timeline Forensics Preventation
+
+## usage
+
+set `API_ID` & `API_HASH` then `python main.py`
--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+import asyncio
+import uvloop
+import json
+import time
+import logging
+import os
+import random
 
+from pyrogram.raw import functions, types
+from pyrogram import Client
+
+CHECK_INTERVAL = 16
+POSSIBLE = 0.95
+
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_hash')
+
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+async def main():
+    app = Client("user", api_id=api_id, api_hash=api_hash)
+    async with app:
+        while True:
+            time.sleep(CHECK_INTERVAL)
+            status = json.loads((await app.get_me()).__str__())["status"]
+
+            if status == "UserStatus.OFFLINE":
+                logging.warning('Offline detected. Online ing')
+                await app.invoke(functions.account.UpdateStatus(offline=False))
+            else:
+                if random.random() > POSSIBLE:
+                    logging.info('Online. Randomly set offline')
+                    await app.invoke(functions.account.UpdateStatus(offline=True))
+
+uvloop.install()
+asyncio.run(main())
