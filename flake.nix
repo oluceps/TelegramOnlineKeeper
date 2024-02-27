fbe74ac9e8cad18a4867dbc4a37a622e67413776
oluceps <i@oluceps.uk>
2024-02-27 14:49:07 +0800

    + module

--- flake.nix
+++ flake.nix
@@ -13,18 +13,16 @@
   };
 
   outputs = inputs@{ flake-parts, devenv, poetry2nix, nixpkgs, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+    flake-parts.lib.mkFlake { inherit inputs; } ({ moduleWithSystem, ... }: {
       imports = [
         inputs.devenv.flakeModule
       ];
       systems = [ "x86_64-linux" "aarch64-linux" ];
       perSystem = { config, self', inputs', pkgs, system, ... }:
-        let
-          pkgs = import nixpkgs { system = "x86_64-linux"; };
-          inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
-        in
         {
-          packages.default = mkPoetryApplication {
+          packages.default = (poetry2nix.lib.mkPoetry2Nix {
+            inherit pkgs;
+          }).mkPoetryApplication {
             projectDir = ./.;
           };
 
@@ -41,5 +39,63 @@
             '';
           };
         };
-    };
+
+      flake = {
+        nixosModules.default =
+          moduleWithSystem
+            (
+              perSystem@{ config }:
+              { lib, ... }: {
+                imports = [
+                  ({ lib, config, ... }:
+                    with lib;
+                    let
+                      cfg = config.services.online-keeper;
+                    in
+                    {
+                      options.services.online-keeper = {
+                        instances = mkOption {
+                          type = types.listOf (types.submodule {
+                            options = {
+                              name = mkOption { type = types.str; };
+                              package = mkPackageOption perSystem.config.packages "default" { };
+                              environmentFile = mkOption { type = types.str; default = ""; };
+                            };
+                          });
+                          default = [ ];
+                        };
+                      };
+                      config =
+                        mkIf (cfg.instances != [ ])
+                          {
+                            environment.systemPackages = lib.unique (lib.foldr
+                              (s: acc: acc ++ [ s.package ]) [ ]
+                              cfg.instances);
+
+                            systemd.services = lib.foldr
+                              (s: acc: acc // {
+                                "online-keeper-${s.name}" = {
+                                  wantedBy = [ "multi-user.target" ];
+                                  after = [ "network-online.target" ];
+                                  wants = [ "network-online.target" ];
+                                  description = "telegram online-keeper daemon";
+                                  serviceConfig = {
+                                    DynamicUser = true;
+                                    ExecStart = lib.getExe' s.package "online-keeper";
+                                    EnvironmentFile = s.environmentFile;
+                                    Restart = "on-failure";
+                                  };
+                                };
+                              })
+                              { }
+                              cfg.instances;
+                          };
+                    })
+                ];
+
+              }
+
+            );
+      };
+    });
 }
