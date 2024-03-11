f7030c571faa8afa83920d8852e38f52c7768caf
oluceps <i@oluceps.uk>
2024-03-11 18:41:58 +0800

    ~ enhance module

--- flake.nix
+++ flake.nix
@@ -49,59 +49,53 @@
           moduleWithSystem
             (
               perSystem@{ config }:
-              { lib, ... }: {
-                imports = [
-                  ({ lib, config, ... }:
-                    with lib;
-                    let
-                      cfg = config.services.online-keeper;
-                    in
-                    {
-                      options.services.online-keeper = {
-                        instances = mkOption {
-                          type = types.listOf (types.submodule {
-                            options = {
-                              name = mkOption { type = types.str; };
-                              package = mkPackageOption perSystem.config.packages "default" { };
-                              sessionFile = mkOption { type = types.str; default = ""; };
-                              environmentFile = mkOption { type = types.str; default = ""; };
-                            };
-                          });
-                          default = [ ];
+              nixos@{ lib, ... }:
+                with lib;
+                let
+                  cfg = nixos.config.services.online-keeper;
+                in
+                {
+                  options.services.online-keeper = {
+                    instances = mkOption {
+                      type = types.listOf (types.submodule {
+                        options = {
+                          name = mkOption { type = types.str; };
+                          package = mkPackageOption perSystem.config.packages "default" { };
+                          sessionFile = mkOption { type = types.str; default = ""; };
+                          environmentFile = mkOption { type = types.str; default = ""; };
                         };
+                      });
+                      default = [ ];
+                    };
+                  };
+                  config =
+                    mkIf (cfg.instances != [ ])
+                      {
+                        environment.systemPackages = lib.unique (lib.foldr
+                          (s: acc: acc ++ [ s.package ]) [ ]
+                          cfg.instances);
+
+                        systemd.services = lib.foldr
+                          (s: acc: acc // {
+                            "online-keeper-${s.name}" = {
+                              wantedBy = [ "multi-user.target" ];
+                              after = [ "network-online.target" ];
+                              wants = [ "network-online.target" ];
+                              description = "telegram online-keeper daemon";
+                              serviceConfig = {
+                                DynamicUser = true;
+                                LoadCredential = [ "session:${s.sessionFile}" ];
+                                Environment = [ "SESSION_FILE=/run/credentials/online-keeper-${s.name}.service/session" ];
+                                EnvironmentFile = s.environmentFile;
+                                ExecStart = lib.getExe' s.package "tg-online-keeper";
+                                Restart = "on-failure";
+                              };
+                            };
+                          })
+                          { }
+                          cfg.instances;
                       };
-                      config =
-                        mkIf (cfg.instances != [ ])
-                          {
-                            environment.systemPackages = lib.unique (lib.foldr
-                              (s: acc: acc ++ [ s.package ]) [ ]
-                              cfg.instances);
-
-                            systemd.services = lib.foldr
-                              (s: acc: acc // {
-                                "online-keeper-${s.name}" = {
-                                  wantedBy = [ "multi-user.target" ];
-                                  after = [ "network-online.target" ];
-                                  wants = [ "network-online.target" ];
-                                  description = "telegram online-keeper daemon";
-                                  serviceConfig = {
-                                    DynamicUser = true;
-                                    LoadCredential = [ "session:${s.sessionFile}" ];
-                                    Environment = [ "SESSION_FILE=/run/credentials/online-keeper-${s.name}.service/session" ];
-                                    EnvironmentFile = s.environmentFile;
-                                    ExecStart = lib.getExe' s.package "tg-online-keeper";
-                                    Restart = "on-failure";
-                                  };
-                                };
-                              })
-                              { }
-                              cfg.instances;
-                          };
-                    })
-                ];
-
-              }
-
+                }
             );
       };
     });
